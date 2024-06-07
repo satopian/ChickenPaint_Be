5479b1d8ecaad0da407150b54d02c31f5ae17754
satopian <satopian@mia.eek.jp>
2024-06-07 21:02:29 +0900

    パレット表示非表示ショートカットメニューを押しやすくした。ブラウザのホームボタン誤ってタップが発生しないUIにした。

--- js/gui/CPMainMenu.js
+++ js/gui/CPMainMenu.js
@@ -426,10 +426,10 @@ export default function CPMainMenu(controller, mainGUI) {
     let
 	bar = $(
 		'<nav class="navbar navbar-expand-md navbar-light bg-light">'
-			+ '<a class="navbar-brand" href="#">ChickenPaint Be</a>'
+			+ '<div class="navbar-upper"><a class="navbar-brand" href="#">ChickenPaint Be</a>'
 			+ '<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#chickenpaint-main-menu-content" aria-controls="chickenpaint-main-menu-content" aria-expanded="false" aria-label="Toggle main menu">'
 				+ '<span class="navbar-toggler-icon"></span>'
-			+ '</button>'
+			+ '</button></div>'
 			+ '<div class="collapse navbar-collapse" id="chickenpaint-main-menu-content">'
 				+ '<div class="navbar-nav mr-auto">'
 				+ '</div>'
