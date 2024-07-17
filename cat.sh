3e433bb2d78d57c98ea403a6d9dcc662b7ba6c00
satopian <satopian@mia.eek.jp>
2024-07-17 14:28:33 +0900

    cat.sh min.shを削除　makefileに機能を統合。

--- cat.sh
+++
@@ -1,2 +0,0 @@
-mkdir -p dest
-cat "bootstrap/js/header.txt" "bootstrap/js/bootstrap.bundle.min.js" "resources/js/chickenpaint.js" > "dest/chickenpaint.js"
--- min.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p temp
-google-closure-compiler --js dest/chickenpaint.js --js_output_file temp/chickenpaint.temp.min.js
-cat "bootstrap/js/header.txt" "temp/chickenpaint.temp.min.js" > "dest/chickenpaint.min.js"
