4be88f53371c1d74bbecf3c0bb6999b6f709cab3
satopian <satopian@mia.eek.jp>
2026-02-19 18:58:35 +0900

    ブラウザの警告がでるのでYieldの回数を増やした。

--- js/engine/CPChibiFile.js
+++ js/engine/CPChibiFile.js
@@ -785,7 +785,7 @@ export function save(artwork, options = {}) {
                 chunks.push(serializeLayerChunk(layer));
 
                 const shouldYield =
-                    !savedb || savedbFromMenu ? (i + 1) % 16 === 0 : true;
+                    !savedb || savedbFromMenu ? (i + 1) % 3 === 0 : true;
                 if (shouldYield) {
                     await yieldToMain();
                 }
@@ -810,7 +810,7 @@ export function save(artwork, options = {}) {
                 { level: savedb ? 1 : 6 },
             );
 
-            const step = (!savedb || savedbFromMenu ? 1024 : 128) * 1024;
+            const step = (!savedb || savedbFromMenu ? 512 : 128) * 1024;
             for (let i = 0; i < uncompressedData.length; i += step) {
                 const isLast = i + step >= uncompressedData.length;
                 deflator.push(uncompressedData.subarray(i, i + step), isLast);
