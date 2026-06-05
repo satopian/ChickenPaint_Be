2247c5504f30d57323c197cc834524c27104f29f
satopian <satopian@mia.eek.jp>
2026-06-05 22:44:19 +0900

    BzとカラーセットのUtilityをclass化。

--- js/util/AdobeColorTable.js
+++ js/util/AdobeColorTable.js
@@ -12,74 +12,76 @@ var ACO_COLORSPACE_RGB = 0,
   ACO_COLORSPACE_LAB = 7,
   ACO_COLORSPACE_GRAYSCALE = 8;
 
-export default function AdobeColorTable() {}
+export default class AdobeColorTable {
+  constructor() {}
 
-/**
- * Read an .aco (Adobe COlor) swatches file and return an array of RGB colors.
- *
- * Supports version 1 palettes, only RGB format.
- *
- * @param {Uint8Array} input - The .aco file contents
- * @returns {?Object[]} An array of colours, or null if the file was not supported.
- */
-AdobeColorTable.prototype.read = function (input) {
-  if (input == null) {
-    return null;
-  }
-
-  var stream = new ArrayDataStream(new Uint8Array(input)),
-    version,
-    count,
-    result = [];
-
-  version = stream.readU16BE();
-  if (version != 1) {
-    return null;
-  }
-  count = stream.readU16BE();
-
-  for (var i = 0; i < count; i++) {
-    var colorSpace = stream.readU16BE();
-
-    if (colorSpace != ACO_COLORSPACE_RGB) {
-      continue; // Drop unsupported colours silently
+  /**
+   * Read an .aco (Adobe COlor) swatches file and return an array of RGB colors.
+   *
+   * Supports version 1 palettes, only RGB format.
+   *
+   * @param {Uint8Array} input - The .aco file contents
+   * @returns {?Object[]} An array of colours, or null if the file was not supported.
+   */
+  read(input) {
+    if (input == null) {
+      return null;
     }
 
-    // Scale back down from 16-bit to 8-bit
-    var r = (stream.readU16BE() * 255) / 65535,
-      g = (stream.readU16BE() * 255) / 65535,
-      b = (stream.readU16BE() * 255) / 65535;
+    var stream = new ArrayDataStream(new Uint8Array(input)),
+      version,
+      count,
+      result = [];
 
-    stream.readU16BE(); // third value unused
+    version = stream.readU16BE();
+    if (version != 1) {
+      return null;
+    }
+    count = stream.readU16BE();
 
-    result.push((r << 16) | (g << 8) | b);
+    for (var i = 0; i < count; i++) {
+      var colorSpace = stream.readU16BE();
+
+      if (colorSpace != ACO_COLORSPACE_RGB) {
+        continue; // Drop unsupported colours silently
+      }
+
+      // Scale back down from 16-bit to 8-bit
+      var r = (stream.readU16BE() * 255) / 65535,
+        g = (stream.readU16BE() * 255) / 65535,
+        b = (stream.readU16BE() * 255) / 65535;
+
+      stream.readU16BE(); // third value unused
+
+      result.push((r << 16) | (g << 8) | b);
+    }
+
+    return result;
   }
 
-  return result;
-};
+  /**
+   * Write an .aco (Adobe COlor) swatches file of the given array of RGB colours (colors are integers with the
+   * blue channel in the least-significant position).
+   */
+  write(colours) {
+    var buffer = new Uint8Array(2 * 2 + colours.length * 10),
+      stream = new ArrayDataStream(buffer);
 
-/**
- * Write an .aco (Adobe COlor) swatches file of the given array of RGB colours (colors are integers with the
- * blue channel in the least-significant position).
- */
-AdobeColorTable.prototype.write = function (colours) {
-  var buffer = new Uint8Array(2 * 2 + colours.length * 10),
-    stream = new ArrayDataStream(buffer);
+    stream.writeU16BE(1); // Version 1
+    stream.writeU16BE(colours.length); // Number of colours
 
-  stream.writeU16BE(1); // Version 1
-  stream.writeU16BE(colours.length); // Number of colours
+    for (var i = 0; i < colours.length; i++) {
+      var colour = colours[i];
 
-  for (var i = 0; i < colours.length; i++) {
-    var colour = colours[i];
+      stream.writeU16BE(ACO_COLORSPACE_RGB);
 
-    stream.writeU16BE(ACO_COLORSPACE_RGB);
+      // Scale up colours to 16-bits (65535/255 = 257)
+      stream.writeU16BE(((colour >> 16) & 0xff) * 257);
+      stream.writeU16BE(((colour >> 8) & 0xff) * 257);
+      stream.writeU16BE((colour & 0xff) * 257);
+      stream.writeU16BE(0);
+    }
 
-    // Scale up colours to 16-bits (65535/255 = 257)
-    stream.writeU16BE(((colour >> 16) & 0xff) * 257);
-    stream.writeU16BE(((colour >> 8) & 0xff) * 257);
-    stream.writeU16BE((colour & 0xff) * 257);
-    stream.writeU16BE(0);
+    return stream.getAsDataArray();
   }
-
-  return stream.getAsDataArray();
-};
+}
--- js/util/CPBezier.js
+++ js/util/CPBezier.js
@@ -29,81 +29,83 @@
     along with ChickenPaint. If not, see <http://www.gnu.org/licenses/>.
 */
 
-export default function CPBezier() {
-  // How to use this class:
-  //
-  // 1 - set the 4 points coordinates (x0-3, y0-3)
-  // two options:
-  // 2a - call init() with desired dt then read the current coordinate (Bx, By) and use nextPoint() to compute the
-  // next point
-  // 2b - use one of the "compute" methods to compute the values for the whole curve in one step
-
-  // The 4 points coordinates
-  this.x0 =
-    this.y0 =
-    this.x1 =
-    this.y1 =
-    this.x2 =
-    this.y2 =
-    this.x3 =
-    this.y3 =
-      0.0;
-
-  // used to compute the Bezier curve with the forward differences method
-  let Bx, dBx, ddBx, dddBx, By, dBy, ddBy, dddBy;
-
-  const init = (dt) => {
-    // Implements a fast degree-3 Bezier curve using the forward differences method
+export default class CPBezier {
+  constructor() {
+    // How to use this class:
     //
-    // Reference for this algorithm:
-    // "Curves and Surfaces for Computer Graphics" by David Salomon, page 189
-    let q1 = 3.0 * dt,
-      q2 = q1 * dt,
-      q3 = dt * dt * dt,
-      q4 = 2.0 * q2,
-      q5 = 6.0 * q3,
-      q6x = this.x0 - 2.0 * this.x1 + this.x2,
-      q6y = this.y0 - 2.0 * this.y1 + this.y2,
-      q7x = 3.0 * (this.x1 - this.x2) - this.x0 + this.x3,
-      q7y = 3.0 * (this.y1 - this.y2) - this.y0 + this.y3;
+    // 1 - set the 4 points coordinates (x0-3, y0-3)
+    // two options:
+    // 2a - call init() with desired dt then read the current coordinate (Bx, By) and use nextPoint() to compute the
+    // next point
+    // 2b - use one of the "compute" methods to compute the values for the whole curve in one step
 
-    Bx = this.x0;
-    By = this.y0;
+    // The 4 points coordinates
+    this.x0 =
+      this.y0 =
+      this.x1 =
+      this.y1 =
+      this.x2 =
+      this.y2 =
+      this.x3 =
+      this.y3 =
+        0.0;
 
-    dBx = (this.x1 - this.x0) * q1 + q6x * q2 + q7x * q3;
-    dBy = (this.y1 - this.y0) * q1 + q6y * q2 + q7y * q3;
+    // used to compute the Bezier curve with the forward differences method
+    let Bx, dBx, ddBx, dddBx, By, dBy, ddBy, dddBy;
 
-    ddBx = q6x * q4 + q7x * q5;
-    ddBy = q6y * q4 + q7y * q5;
+    const init = (dt) => {
+      // Implements a fast degree-3 Bezier curve using the forward differences method
+      //
+      // Reference for this algorithm:
+      // "Curves and Surfaces for Computer Graphics" by David Salomon, page 189
+      let q1 = 3.0 * dt,
+        q2 = q1 * dt,
+        q3 = dt * dt * dt,
+        q4 = 2.0 * q2,
+        q5 = 6.0 * q3,
+        q6x = this.x0 - 2.0 * this.x1 + this.x2,
+        q6y = this.y0 - 2.0 * this.y1 + this.y2,
+        q7x = 3.0 * (this.x1 - this.x2) - this.x0 + this.x3,
+        q7y = 3.0 * (this.y1 - this.y2) - this.y0 + this.y3;
 
-    dddBx = q7x * q5;
-    dddBy = q7y * q5;
-  };
+      Bx = this.x0;
+      By = this.y0;
 
-  /**
-   * Fill the given x,y arrays with a series of points on the curve.
-   *
-   * @param {Number[]} x
-   * @param {Number[]} y
-   *
-   * @param {number} elements Count of elements to fill x and y arrays
-   */
-  this.compute = (x, y, elements) => {
-    init(1.0 / elements);
+      dBx = (this.x1 - this.x0) * q1 + q6x * q2 + q7x * q3;
+      dBy = (this.y1 - this.y0) * q1 + q6y * q2 + q7y * q3;
 
-    x[0] = Bx;
-    y[0] = By;
+      ddBx = q6x * q4 + q7x * q5;
+      ddBy = q6y * q4 + q7y * q5;
 
-    for (let i = 1; i < elements; i++) {
-      Bx += dBx;
-      By += dBy;
-      dBx += ddBx;
-      dBy += ddBy;
-      ddBx += dddBx;
-      ddBy += dddBy;
+      dddBx = q7x * q5;
+      dddBy = q7y * q5;
+    };
 
-      x[i] = Bx;
-      y[i] = By;
-    }
-  };
+    /**
+     * Fill the given x,y arrays with a series of points on the curve.
+     *
+     * @param {Number[]} x
+     * @param {Number[]} y
+     *
+     * @param {number} elements Count of elements to fill x and y arrays
+     */
+    this.compute = (x, y, elements) => {
+      init(1.0 / elements);
+
+      x[0] = Bx;
+      y[0] = By;
+
+      for (let i = 1; i < elements; i++) {
+        Bx += dBx;
+        By += dBy;
+        dBx += ddBx;
+        dBy += ddBy;
+        ddBx += dddBx;
+        ddBy += dddBy;
+
+        x[i] = Bx;
+        y[i] = By;
+      }
+    };
+  }
 }
