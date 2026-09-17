2440ca0174ea1432fb505f3206e4ba4eb7c30fe6
satopian <satopian@mia.eek.jp>
2026-09-17 22:43:03 +0900

    水彩ブラシで混色した時の明るさの保持率を98%から99%にして混色時の明度低下くすみを防止。

--- js/engine/CPBrushTool.js
+++ js/engine/CPBrushTool.js
@@ -1631,7 +1631,7 @@ export class CPBrushToolOil extends CPBrushToolDirectBrush {
         const bMixed = b1L + (b2L * invAlpha - b1L * invAlpha) / 255;
 
         // 明るさの保持率
-        const BRIGHTNESS_RETENTION = 0.98;
+        const BRIGHTNESS_RETENTION = 0.99;
 
         brushData[srcOffset] =
           (newAlpha << 24) |
