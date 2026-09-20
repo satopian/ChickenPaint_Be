245846929e93cca88ba0f9b401cd16917011da12
satopian <satopian@mia.eek.jp>
2026-09-20 19:50:04 +0900

    水彩ブラシの不透明度を60から50へ。補充量を30から33へ。

--- js/ChickenPaint.js
+++ js/ChickenPaint.js
@@ -195,7 +195,7 @@ function createDrawingTools() {
   tools[ChickenPaint.T_WATER] = new CPBrushInfo({
     toolNb: ChickenPaint.T_WATER,
     size: 30,
-    alpha: 60,
+    alpha: 50,
     isAA: false,
     minSpacing: 0.5,
     spacing: 0.03,
@@ -205,7 +205,7 @@ function createDrawingTools() {
     brushMode: CPBrushInfo.BRUSH_MODE_WATER,
     paintMode: CPBrushInfo.PAINT_MODE_FLOW,
     alphaScale: 1 / 8,
-    resat: 0.3,
+    resat: 0.33,
     bleed: 0.6,
   });
 
